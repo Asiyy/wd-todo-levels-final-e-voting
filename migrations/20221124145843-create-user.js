9260ba3f5511ab47372aa1037a2398cd2608b7b7
Asiyy <111074697+Asiyy@users.noreply.github.com>
2023-01-07 17:27:08 +0530

    Add files via upload

--- migrations/20221124145843-create-user.js
+++ migrations/20221124145843-create-user.js
@@ -1,22 +1,26 @@
 "use strict";
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("Todos", {
+    await queryInterface.createTable("Users", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER,
       },
-      title: {
+      firstName: {
         type: Sequelize.STRING,
       },
-      dueDate: {
-        type: Sequelize.DATEONLY,
+      lastName: {
+        type: Sequelize.STRING,
       },
-      completed: {
-        type: Sequelize.BOOLEAN,
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: Sequelize.STRING,
       },
       createdAt: {
         allowNull: false,
@@ -29,6 +33,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("Todos");
+    await queryInterface.dropTable("Users");
   },
 };
---
+++ migrations/20221124150424-add-user-id-in-todos.js
@@ -0,0 +1,34 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addColumn("Todos", "userID", {
+      type: Sequelize.DataTypes.INTEGER,
+    });
+
+    await queryInterface.addConstraint("Todos", {
+      fields: ["userID"],
+      type: "foreign key",
+      references: {
+        table: "Users",
+        field: "id",
+      },
+    });
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.removeColumn("Todos", "userID");
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  },
+};
